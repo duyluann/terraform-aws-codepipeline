e097d44594eb9cda6793439d8882fcf68885a770
Nguyen Duy Luan <33739713+duyl97@users.noreply.github.com>
2024-09-04 09:31:03 +0000

    ⚡️ Update buildspec

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "region" {
   description = "The region to deploy the resources"
-  type = string
-  default = "ap-southeast-1"
+  type        = string
+  default     = "ap-southeast-1"
 }
 variable "env" {
   description = "The environment to deploy the resources"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 
 variable "prefix" {
   description = "The prefix for all resource's names"
-  type = string
+  type        = string
 }
